856cfebe0092ca816c8dbd373ac39f7e1c9ce45f
semih <semihotun1@gmail.com>
2024-07-28 14:57:07 +0300

    change grissettingbyid to gridsettingbypath

--- Services/eCommerceBase/eCommerceBase/eCommerceBase.Application/Handlers/GridSettings/Queries/GetGridSettingByPathQuery.cs
+++ Services/eCommerceBase/eCommerceBase/eCommerceBase.Application/Handlers/GridSettings/Queries/GetGridSettingByPathQuery.cs
@@ -5,20 +5,20 @@ using eCommerceBase.Persistence.GenericRepository;
 using eCommerceBase.Insfrastructure.Utilities.Caching.Redis;
 
 namespace eCommerceBase.Application.Handlers.GridSettings.Queries;
-public record GetGridSettingByIdQuery(System.Guid Id) : IRequest<Result<GridSetting>>;
-public class GetGridSettingByIdQueryHandler(IReadDbRepository<GridSetting> gridSettingRepository,
-		ICacheService cacheService) : IRequestHandler<GetGridSettingByIdQuery,
+public record GetGridSettingByPathQuery(string Path) : IRequest<Result<GridSetting>>;
+public class GetGridSettingByPathQueryHandler(IReadDbRepository<GridSetting> gridSettingRepository,
+		ICacheService cacheService) : IRequestHandler<GetGridSettingByPathQuery,
 		Result<GridSetting>>
 {
     private readonly IReadDbRepository<GridSetting> _gridSettingRepository = gridSettingRepository;
     private readonly ICacheService _cacheService = cacheService;
-    public async Task<Result<GridSetting>> Handle(GetGridSettingByIdQuery request,
+    public async Task<Result<GridSetting>> Handle(GetGridSettingByPathQuery request,
 		CancellationToken cancellationToken)
     {
         return await _cacheService.GetAsync(request,
 		async () =>
         {
-            var query = await _gridSettingRepository.GetByIdAsync(request.Id);
+            var query = await _gridSettingRepository.GetAsync(x=>x.Path == request.Path);
             return Result.SuccessDataResult<GridSetting>(query!);
         },
 		cancellationToken);
