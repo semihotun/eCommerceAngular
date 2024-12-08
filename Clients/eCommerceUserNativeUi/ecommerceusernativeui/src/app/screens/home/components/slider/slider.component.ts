23d797f549a44f48e06192e1d0db798f391bf58a
semih <41797901+semihotun@users.noreply.github.com>
2024-12-09 00:06:46 +0300

    Change Slider Theme

--- Clients/eCommerceUserNativeUi/ecommerceusernativeui/src/app/screens/home/components/slider/slider.component.ts
+++ Clients/eCommerceUserNativeUi/ecommerceusernativeui/src/app/screens/home/components/slider/slider.component.ts
@@ -13,6 +13,8 @@ import { HomeService } from 'src/app/services/home.service';
 import { HomeStore } from 'src/app/stores/home.store';
 import { Swiper } from 'swiper/types';
 import { LazyImgDirective } from './../../../../directives/lazy-img.directive';
+import { Slider } from 'src/app/models/responseModel/Slider';
+import { GlobalService } from 'src/app/services/core/global.service';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
@@ -21,21 +23,65 @@ import { LazyImgDirective } from './../../../../directives/lazy-img.directive';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [CommonModule, TranslateModule, LazyImgDirective],
 })
-export class SliderComponent implements AfterViewInit, OnInit {
-  @ViewChild('swiper') swiperRef: ElementRef | undefined;
-  swiper?: Swiper;
-  homeService = inject(HomeService);
+export class SliderComponent implements OnInit {
+  constructor(public glb: GlobalService) {}
+  @ViewChild('sliderWrapper') sliderWrapper!: ElementRef;
+  currentIndex = 0;
+  startX = 0;
+  endX = 0;
   homeStore = inject(HomeStore);
+  autoplayInterval: any;
   ngOnInit(): void {}
-  goNext() {
-    this.swiper?.slideNext();
+  ngAfterViewInit(): void {
+    const slider = this.sliderWrapper.nativeElement;
+    slider.addEventListener('touchstart', this.onTouchStart.bind(this));
+    slider.addEventListener('touchmove', this.onTouchMove.bind(this));
+    slider.addEventListener('touchend', this.onTouchEnd.bind(this));
+    this.startAutoplay();
   }
-  goPrev() {
-    this.swiper?.slidePrev();
+  startAutoplay(): void {
+    this.autoplayInterval = setInterval(() => {
+      this.goNext();
+    }, 4000);
   }
-  ngAfterViewInit(): void {}
-  swiperInit() {
-    this.swiper = this.swiperRef?.nativeElement.swiper;
-    this.swiper?.autoplay.start();
+  stopAutoplay(): void {
+    if (this.autoplayInterval) {
+      clearInterval(this.autoplayInterval);
+    }
+  }
+  onTouchStart(event: TouchEvent) {
+    this.startX = event.touches[0].clientX;
+  }
+
+  onTouchMove(event: TouchEvent) {
+    this.endX = event.touches[0].clientX;
+  }
+
+  onTouchEnd() {
+    if (this.startX > this.endX + 50) {
+      this.goNext();
+    } else if (this.startX < this.endX - 50) {
+      this.goPrev();
+    }
+  }
+
+  get transformStyle(): string {
+    return `translateX(-${this.currentIndex * 100}%)`;
+  }
+
+  goNext(): void {
+    if (this.currentIndex < this.homeStore.sliders$().length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  goPrev(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.homeStore.sliders$().length - 1;
+    }
   }
 }
