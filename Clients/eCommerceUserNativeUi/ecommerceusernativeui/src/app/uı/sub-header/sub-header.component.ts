b06e95a185fdb0afe41b5c63a00cedd28e8fb6fc
semih <41797901+semihotun@users.noreply.github.com>
2024-12-15 01:45:27 +0300

    Suh header component addable button

--- Clients/eCommerceUserNativeUi/ecommerceusernativeui/src/app/uı/sub-header/sub-header.component.ts
+++ Clients/eCommerceUserNativeUi/ecommerceusernativeui/src/app/uı/sub-header/sub-header.component.ts
@@ -1,26 +1,44 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GlobalService } from 'src/app/services/core/global.service';
 import { NavController } from '@ionic/angular/standalone';
+
 @Component({
   selector: 'app-sub-header',
   templateUrl: './sub-header.component.html',
   styleUrls: ['./sub-header.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
 })
 export class SubHeaderComponent implements OnInit {
+  @ContentChild('customButton', { static: false })
+  customButton!: ElementRef<HTMLButtonElement>;
   @Input() previousUrl: string | null = null;
   @Input() title: string = '';
   router = inject(Router);
   glb = inject(GlobalService);
   navController = inject(NavController);
+
   constructor() {}
+
+  ngOnInit() {}
+  ngAfterViewInit() {
+    if (this.customButton) {
+      this.customButton.nativeElement.addEventListener('click', () => {});
+    }
+  }
 
   routerNavigate() {
     this.navController.navigateBack(this.previousUrl!);
   }
-  ngOnInit() {}
 }
